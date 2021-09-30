169a9c51fd9cfe7e5e751006a0bd5bfca2666c7d
iCAROS7 <hominlab@gmail.com>
2021-09-30 21:08:08 +0900

    PE_5-2 Airplane 객체 print() 출력 양식 변경

--- PE_5-2/PE_5-2/main.cpp
+++ PE_5-2/PE_5-2/main.cpp
@@ -35,19 +35,15 @@ public:
 };
 
 // 비행기 정보 출력 메서드
-void Airplane::print() { cout << "비행기의 이름: " << name << "\n비행기의 용량: " << capacity << "\n비행기의 속도: " << speed << " Km/h" << endl; }
+void Airplane::print() { cout << "비행기의 이름: " << name << "\n비행기의 용량: " << capacity << "\n비행기의 속도: " << speed << " Km/h\n" << endl; }
 
 int main() {
     Airplane plane1("보잉 787", 900, 300); // 매개변수와 함께 plane1 객체 생성
-    
     cout << "비행기 #1" << endl;
     plane1.print(); // plane1 정보 출력
     
-    Airplane plane2; // plane2 객체 생성
-    plane2.setName("에어버스 350");
-    plane2.setSpeed(1000);
+    Airplane plane2("에어버스 350", 1000); // 매개변수와 함께 plane2 객체 생성
     plane2.setCapacity(400);
-    
     cout << "비행기 #2" << endl;
     plane2.print();
     
