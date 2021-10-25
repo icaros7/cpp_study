a1b0eddf4d1d283d1224f59468822608b712354d
iCAROS7 <hominlab@gmail.com>
2021-10-25 22:12:58 +0900

    동적 메모리 해제 메서드 추가

--- PE_9-2/main.cpp
+++ PE_9-2/main.cpp
@@ -11,6 +11,7 @@ class Line {
 public:
     Line(int len);
     Line(const Line &line);
+    void deletePtr(); // 동적 할당 메모리 해제를 위한 메서드
     int getLength(void) { return *ptr; } // 선의 길이 getter
 
 private:
@@ -30,6 +31,11 @@ Line::Line(const Line &line) {
     this->ptr = line.ptr;
 }
 
+// 동적 할당 메모리 해제를 위한 메서드
+void Line::deletePtr() {
+    delete ptr;
+}
+
 int main() {
     Line line1(10); // 길이가 10인 일반 생성자 호출
     Line line2(line1); // line1 복사 생성자 호출
@@ -38,6 +44,11 @@ int main() {
 
     Line line4(line3); // line3 복사 생성자 호출
     cout << "선의 길이: " << line4.getLength() << endl;
+
+    /* line4.ptr -> line3.ptr -> line2.ptr -> line1.ptr 을
+     * 가르키고 있으므로 line1.ptr 만 delete 호출
+     * 소멸자를 이용하여 delete 호출시, line1.ptr을 4번 delete 시도를 하여 오류 발생 */
+    line1.deletePtr();
 
     return 0;
 }
