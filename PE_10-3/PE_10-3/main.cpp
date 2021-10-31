0cc4f4176b1d1df74b3446373d30889d856ddc6f
iCAROS7 <hominlab@gmail.com>
2021-10-31 15:23:45 +0900

    PE_10-3 추가

---
+++ PE_10-3/PE_10-3/main.cpp
@@ -0,0 +1,45 @@
+/*
+ * PE: 10-3
+ * 작성: 201735030 이호민
+ * 파일: main.cpp
+ */
+
+#include <iostream>
+
+using namespace std;
+
+// Box Class
+class Box {
+private:
+    double length; // 박스의 길이
+    double width; // 박스의 가로
+    double height; // 박스의 세로
+    
+public:
+    Box(int length = 0, int width = 0, int height = 0) : length(length), width(width), height(height) {} // 생성자
+    
+    double getVolume(void) { return length * height * width; } // 부피 반환 메서드, 매개변수 전달 방지
+    
+    // < 연산자 오버라이드, this 상자 부피 < slave 상자 부피 연산 반환
+    bool operator< (Box &slave) { return (getVolume() < slave.getVolume()); }
+    
+    // 상자의 정보 출력 메서드
+    // int boxNum: int 형 상자의 순서 정보
+    void printBox(int boxNum) {
+        cout << "상자 #" << boxNum << endl;
+        cout << "상자의 길이: " << this->length << endl;
+        cout << "상자의 너비: " << this->width << endl;
+        cout << "상자의 높이: " << this->height << endl;
+        cout << "상자의 부피: " << getVolume() << "\n" << endl;
+    }
+};
+
+int main() {
+    Box a(10,10,10), b(20,20,20);
+    
+    a.printBox(1);
+    b.printBox(2);
+    cout << ( a < b) << endl;
+    
+    return 0;
+}
