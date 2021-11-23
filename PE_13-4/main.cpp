5152487c55e1fdf31823131b20f6f93aad059db2
iCAROS7 <hominlab@gmail.com>
2021-11-24 00:17:42 +0900

    과제 필요 조건 적용

--- PE_13-4/main.cpp
+++ PE_13-4/main.cpp
@@ -5,21 +5,24 @@
  */
 #include <iostream>
 #include <fstream>
-#include <vector>
 
 using namespace std;
 
 int main() {
     ifstream is{ "input.txt" };
-    if (!is) { // 파일 열기 실패시
+    if (!is) {      // 파일 열기 실패시
         cerr << "파일 오픈에 실패하였습니다." << endl;
-        exit(1); // 1과 함께 종료
+        exit(1);     // 1과 함께 종료
     }
 
-    vector<char> v; // char 형 벡터 생성
-    char c; // 임시 문자열 생성
-    while (is.get(c)) { v.insert(v.begin(), c); } // v 맨 앞에 읽은 c insert
-    for (auto& i : v) { cout << i; } // v 벡터 출력
+    is.seekg(0, ios::end);  // 커서 위치를 가장 끝으로 이동
+    int num = is.tellg();   // 현재 커서 위치를 파일의 크기로 간주하고 저장
+    char *c = new char[num];// char 형 동적 배열 생성
+    is.clear();             // 이전 값 clear
+    is.seekg(ios::beg);     // 커서 위치를 가장 앞으로 이동
+
+    for (int i = num - 1; i >= 0; i--) { is.get(c[i]); } // 동적 배열 뒷 요소부터 저장
+    for (int i = 0; i < num; i++) { cout << c[i]; } // 배열 앞 요소부터 출력
 
     return 0;
 }
